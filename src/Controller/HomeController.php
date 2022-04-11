73d7ee3fa797c2c9dc0af0175cf602443d6602f8
AlainNiessen <60482327+AlainNiessen@users.noreply.github.com>
2022-04-11 14:04:33 +0200

    Installation Symfony + WebpackEncore, Configuration Webpack Encore + Test affichage

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomeController extends AbstractController
+{
+    /**
+     * @Route("/", name="home")
+     */
+    public function index(): Response
+    {
+        $test_name = "Voix ambi";
+
+        return $this->render('home/index.html.twig', [
+            'test_name' => $test_name,
+        ]);
+    }
+}
