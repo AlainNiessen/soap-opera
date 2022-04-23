c694ca1160aace5a170384dc3dfafbc99097c2aa
AlainNiessen <60482327+AlainNiessen@users.noreply.github.com>
2022-04-23 12:20:52 +0200

    Installation barre de recherche + premiére test de récupération dans ArticleController

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -2,19 +2,39 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Article;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class ArticleController extends AbstractController
 {
+    //----------------------------------------------
+    // ROUTE RECHERCHE ARTICLES
+    //----------------------------------------------
     /**
-     * @Route("/article", name="app_article")
+     * @Route("/article/recherche", name="article_recherche", methods={"POST", "GET"})
      */
-    public function index(): Response
+    public function requestArticle(Request $request, EntityManagerInterface $entityManager): Response
     {
+        //récupération des données rentrées dans le fomulaire
+        $mots = $request -> request -> get('mots');
+
+        //transform string en tableau
+        $tabWords = explode(" ", trim($mots));
+
+        //passage du tableau à la fonction de requete
+        //définition repository article
+        $repositoryArticle = $entityManager -> getRepository(Article::class);
+        // fonction de requête sur base de données récupérées       
+        $articles = $repositoryArticle -> findArticlesSearchBar();     
+
+        dd($articles);
+
         return $this->render('article/list.html.twig', [
-            'controller_name' => 'ArticleController',
+            'articles' => $articles
         ]);
     }
 }
