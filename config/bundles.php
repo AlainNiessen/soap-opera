9ded7d792e0d99e45f0dac7ceb09f5a824453eea
AlainNiessen <60482327+AlainNiessen@users.noreply.github.com>
2022-04-29 15:49:12 +0200

    Deinstallation KNPPaginator + update composer

--- config/bundles.php
+++ config/bundles.php
@@ -15,5 +15,4 @@ return [
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
     EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
     Vich\UploaderBundle\VichUploaderBundle::class => ['all' => true],
-    Knp\Bundle\PaginatorBundle\KnpPaginatorBundle::class => ['all' => true],
 ];
