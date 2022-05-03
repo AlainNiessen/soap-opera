995d5bc6ebae01139b6ff4583493fc9660a915f6
AlainNiessen <60482327+AlainNiessen@users.noreply.github.com>
2022-05-03 18:40:43 +0200

    correction recherche par catégorie

--- src/Controller/FragmentRechercheController.php
+++ src/Controller/FragmentRechercheController.php
@@ -9,7 +9,6 @@ use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Contracts\Translation\TranslatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class FragmentRechercheController extends AbstractController
@@ -17,7 +16,7 @@ class FragmentRechercheController extends AbstractController
     /**
      * @Route("/fragment/recherche", name="recherche")
      */
-    public function requestAllCategories(EntityManagerInterface $entityManager, TranslatorInterface $translator, Request $request): Response
+    public function requestAllCategories(EntityManagerInterface $entityManager, Request $request): Response
     {
         //récupération langue
         $lang = $request-> getLocale();
