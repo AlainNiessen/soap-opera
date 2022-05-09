ac67f7c9913494e4c97d43ad76d9d159f4e0fe11
AlainNiessen <60482327+AlainNiessen@users.noreply.github.com>
2022-05-09 18:26:46 +0200

    divers test sur inscription pour contrôler adresseHome + adresseDeliver sans double enregistrement dans BD

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use DateTime;
+use App\Entity\Adresse;
 use App\Entity\Utilisateur;
 use App\Form\InscriptionUtilisateurType;
 use Doctrine\ORM\EntityManagerInterface;
@@ -17,7 +18,7 @@ class RegistrationController extends AbstractController
     /**
      * @Route("/registration", name="registration")
      */
-    public function index(Request $request, UserPasswordHasherInterface $encoder, EntityManagerInterface $manager): Response
+    public function index(Request $request, UserPasswordHasherInterface $encoder, EntityManagerInterface $entityManager): Response
     {
         //création nouveau utilisateur
         $utilisateur = new Utilisateur();
@@ -31,7 +32,7 @@ class RegistrationController extends AbstractController
         $formInscription ->handleRequest($request);
 
         //si le formulaire a été submit et valid
-        if($formInscription -> isSubmitted() && $formInscription -> isValid()) {
+        if($formInscription -> isSubmitted() && $formInscription -> isValid()):
             //récupération des données du formulaire
             $utilisateur = $formInscription -> getData();
             // hasher the plain password
@@ -44,9 +45,115 @@ class RegistrationController extends AbstractController
 
             //appel à la fonction privée generateToken pour générer le Token et l'attribuer à l'utilisateur créé dans le formulaire
             $utilisateur -> setInscriptionToken($this -> generateToken());  
-            }
 
-            dd($utilisateur);
+            // récupération de toutes les adresses dans la BD => comparaison si les adresses fournies par le formulaire existent déjà 
+            //définition repository adresse
+            $repositoryAdresse = $entityManager -> getRepository(Adresse::class);
+            // fonction de requête sur base de données récupérées       
+            $adresses = $repositoryAdresse -> findAll();
+            // tableau de réfèrence
+            $tabRefHome = [];
+            $tabRefDeliver = [];
+
+            // vérification si il y en a des adresses dans la BD
+            // si non => boucle sur les adresses
+            if(!empty($adresses)):
+                foreach($adresses as $adresse):
+                    // si l'adresse du formulaire existe déjà 
+                    if( $adresse->getNumeroRue()    == $utilisateur->getAdresseHome()->getNumeroRue()   &&
+                        $adresse->getRue()          == $utilisateur->getAdresseHome()->getRue()         &&
+                        $adresse->getCodePostal()   == $utilisateur->getAdresseHome()->getCodePostal()  &&
+                        $adresse->getVille()        == $utilisateur->getAdresseHome()->getVille()       &&  
+                        $adresse->getPays()         == $utilisateur->getAdresseHome()->getPays()):         
+                            // attribution de l'adresse de la BD au nouveau utilisateur
+                            $utilisateur -> setAdresseHome($adresse);
+                    else:
+                        // stockage de l'adresse dans le tableau de référence
+                        $tabRefHome[] = $adresse;                                         
+                    endif;
+                endforeach;
+                // si l'adresse du formulaire n'existe pas encore
+                if(count($adresses) == count($tabRefHome)):
+                    // => création nouvelle adresse dans la BD et attribution par après à l'utilisateur
+                    $adresseNew = new Adresse();
+                    $adresseNew->setNumeroRue($utilisateur->getAdresseHome()->getNumeroRue());
+                    $adresseNew->setRue($utilisateur->getAdresseHome()->getRue());
+                    $adresseNew->setCodePostal($utilisateur->getAdresseHome()->getCodePostal());
+                    $adresseNew->setVille($utilisateur->getAdresseHome()->getVille());
+                    $adresseNew->setPays($utilisateur->getAdresseHome()->getPays()); 
+
+                    //préparation insertion dans la BD
+                    $entityManager -> persist($adresseNew);
+                    //insertion BD
+                    $entityManager -> flush();
+
+                    $utilisateur -> setAdresseHome($adresseNew);  
+                endif;
+            // si oui => création d'une première adresse
+            else:
+                $adresseNew = new Adresse();
+                $adresseNew->setNumeroRue($utilisateur->getAdresseHome()->getNumeroRue());
+                $adresseNew->setRue($utilisateur->getAdresseHome()->getRue());
+                $adresseNew->setCodePostal($utilisateur->getAdresseHome()->getCodePostal());
+                $adresseNew->setVille($utilisateur->getAdresseHome()->getVille());
+                $adresseNew->setPays($utilisateur->getAdresseHome()->getPays()); 
+
+                //préparation insertion dans la BD
+                $entityManager -> persist($adresseNew);
+                //insertion BD
+                $entityManager -> flush();
+
+                $utilisateur -> setAdresseHome($adresseNew);
+            endif;
+
+            // deuxiéme récupération des adresses pour parcourir le même chemin pour adresseDeliver   
+            $adresses = $repositoryAdresse -> findAll();
+
+            // directement la boucle car la base de données a au moins une adresse
+            foreach($adresses as $adresse):
+                //vérification si adresseDeliver existe déjà dans la BD
+                if( $adresse->getNumeroRue()    == $utilisateur->getAdresseDeliver()->getNumeroRue()   &&
+                    $adresse->getRue()          == $utilisateur->getAdresseDeliver()->getRue()         &&
+                    $adresse->getCodePostal()   == $utilisateur->getAdresseDeliver()->getCodePostal()  &&
+                    $adresse->getVille()        == $utilisateur->getAdresseDeliver()->getVille()       &&  
+                    $adresse->getPays()         == $utilisateur->getAdresseDeliver()->getPays()):         
+                        //si oui => attribution de l'adresse de la BD au nouveau utilisateur
+                        $utilisateur -> setAdresseDeliver($adresse);
+                else:
+                    // stockage de l'adresse dans le tableau de référence
+                    $tabRefDeliver[] = $adresse;                                       
+                endif;
+            endforeach;
+
+            if(count($adresses) == count($tabRefDeliver)):
+                //si non => création nouvelle adresse dans la BD et attribution par après à l'utilisateur
+                $adresseNew = new Adresse();
+                $adresseNew->setNumeroRue($utilisateur->getAdresseDeliver()->getNumeroRue());
+                $adresseNew->setRue($utilisateur->getAdresseDeliver()->getRue());
+                $adresseNew->setCodePostal($utilisateur->getAdresseDeliver()->getCodePostal());
+                $adresseNew->setVille($utilisateur->getAdresseDeliver()->getVille());
+                $adresseNew->setPays($utilisateur->getAdresseDeliver()->getPays()); 
+
+                //préparation insertion dans la BD
+                $entityManager -> persist($adresseNew);
+                //insertion BD
+                $entityManager -> flush();
+
+                $utilisateur -> setAdresseDeliver($adresseNew);
+            endif;
+
+            //préparation insertion dans la BD
+            $entityManager -> persist($utilisateur);
+            //insertion BD
+            $entityManager -> flush();
+        endif;
+
+
+
+            
+
+            
+        
 
        return $this->render('registration/inscriptionUtilisateur.html.twig', [
             'formInscription' => $formInscription -> createView()
