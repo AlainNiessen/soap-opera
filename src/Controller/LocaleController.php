1df19ef4209b43dd521a2825132708bdeaede509
AlainNiessen <60482327+AlainNiessen@users.noreply.github.com>
2022-05-20 18:46:17 +0200

    Ajout systéme de récupération de la langue via drapeau + Création LocaleSubscriber as EventSubscriber

---
+++ src/Controller/LocaleController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class LocaleController extends AbstractController
+{
+    /**
+     * @Route("/locale/{locale}", name="locale")
+     */
+    public function changeLocale($locale, Request $request): Response
+    {
+        // on stocke la langue demandé dans la session
+        $session = $request -> getSession();
+        $session -> set('_locale', $locale);
+
+        //on revient sur la page précedente
+        return $this->redirect($request -> headers -> get('referer'));
+        
+    }
+}
---
+++ src/EventSubscriber/LocaleSubscriber.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\EventSubscriber;
+
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpKernel\Event\RequestEvent;
+use Symfony\Component\HttpKernel\KernelEvents;
+
+class LocaleSubscriber implements EventSubscriberInterface
+{
+    private $defaultLocale;
+
+    public function __construct($defaultLocale = 'de')
+    {
+        $this->defaultLocale = $defaultLocale;
+    }
+
+    public function onKernelRequest(RequestEvent $event)
+    {
+        $request = $event->getRequest();
+        if (!$request->hasPreviousSession()) {
+            return;
+        }
+        // essayez de voir si la locale a été définie en tant que paramètre de routage _locale
+        if ($locale = $request->query->get('_locale')) {
+            $request->setLocale($locale);
+        } else {
+            // si aucune locale explicite n'a été définie sur cette requête, utilisez-en une de la session
+            $request->setLocale($request->getSession()->get('_locale', $this->defaultLocale));
+        }
+    }
+
+    public static function getSubscribedEvents()
+    {
+        return [
+            KernelEvents::REQUEST => [['onKernelRequest', 20]],
+        ];
+    }
+}
